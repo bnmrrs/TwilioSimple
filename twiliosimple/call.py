d03492a7fb45aab7af748804451eb4c0a66987e4
Ben Morris <bmorris@iron.(none)>
2010-08-17 16:35:43 -0400

    Added additional validation and removed TwilioReponse and Call from the loaded response

--- twiliosimple/call.py
+++ twiliosimple/call.py
@@ -30,12 +30,15 @@ import exceptions
 class OutgoingCall:
   def __init__(self, call_response):
     self.raw_response = call_response
-    self.loaded_response = json.loads(call_response)
-
-    self.validate_response(self.loaded_response)
+    self.validated_response = self.validate_response(call_response)
     self.load_response(self.loaded_response)
 
   def validate_response(self, response):
+    try:
+      response = json.loads(response)
+    except ValueError:
+      raise InvalidResponse('Response was not a valid JSON document')
+
     if not 'TwilioResponse' in response:
       raise InvalidResponse('Twilio Response was not included')
 
@@ -45,18 +48,21 @@ class OutgoingCall:
     if not 'Call' in response['TwilioResponse']:
       raise InvalidResponse('Call body was not included in the response')
 
-  def load_response(self, response):
+    return response
 
-    self.call_sid = response['TwilioResponse']['Call']['Sid']
-    self.account_sid = response['TwilioResponse']['Call']['AccountSid']
-    self.called = response['TwilioResponse']['Call']['Called']
-    self.caller = response['TwilioResponse']['Call']['Caller']
-    self.phone_number_sid = response['TwilioResponse']['Call']['PhoneNumberSid']
-    self.status = response['TwilioResponse']['Call']['Status']
-    self.start_time = response['TwilioResponse']['Call']['StartTime']
-    self.end_time = response['TwilioResponse']['Call']['EndTime']
-    self.price = response['TwilioResponse']['Call']['Price']
-    self.flags = response['TwilioResponse']['Call']['Flags']
+  def load_response(self, response):
+    self.loaded_response = {
+        'call_sid': response['TwilioResponse']['Call']['Sid'],
+        'account_sid': response['TwilioResponse']['Call']['AccountSid'],
+        'called': response['TwilioResponse']['Call']['Called'],
+        'caller': response['TwilioResponse']['Call']['Caller'],
+        'phone_number_sid': response['TwilioResponse']['Call']['PhoneNumberSid'],
+        'status': response['TwilioResponse']['Call']['Status'],
+        'start_time': response['TwilioResponse']['Call']['StartTime'],
+        'end_time': response['TwilioResponse']['Call']['EndTime'],
+        'price': response['TwilioResponse']['Call']['Price'],
+        'flags': response['TwilioResponse']['Call']['Flags']
+    }
 
   def get_response(self):
     return self.loaded_response
