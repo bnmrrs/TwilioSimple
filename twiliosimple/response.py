496a699e7ff6a252e3545c3604eb5328fabf6ff3
bmorris <ben@bnmrrs.com>
2010-09-04 12:07:08 -0400

    Totally redid the response internals.  May as well take advantage of the Twilio response

--- twiliosimple/response.py
+++ twiliosimple/response.py
@@ -22,47 +22,118 @@
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 #  THE SOFTWARE.
 
+import twilio as twilio_official
 
 class Response:
-	content = ""
 	
-	def say(self, to_say, voice='woman', language='en', loop=0):
-		self.content += "<Say voice='%s' loop='%d' language='%s'>%s</Say>" % \
-			(voice, loop, language, to_say)
+	VOICE_WOMAN = 'woman'
+	VOICE_MAN = 'man'
+	
+	LANGUAGE_ENGLISH = 'en'
+	LANGUAGE_SPANISH = 'es'
+	LANGUAGE_FRENCH = 'fr'
+	LANGUAGE_GERMAN = 'de'
+	
+	HTTP_METHOD_POST = 'POST'
+	HTTP_METHOD_GET = 'GET'
+	
+	REASON_REJECTED = 'rejected'
+	REASON_BUSY = 'busy'
+	
+	def __init__(self):
+		self.twilio_response = twilio_official.Response()
 		
-		return self
-		
-	def play(self, to_play, loop=0):
-		self.content += "<Play loop='%d'>%s</Play>" % (loop, to_play)
-		return self
-		
-	def redirect_get(self):
-		raise NotImplementedError
-		
-	def gather(self, action_url, num_digits=1, finish_on_key='#', timeout=5, method='POST'):
-		self.content += "<Gather action='%s' numDigits='%d' finishOnKey='%s' timeout='%d' method='%s' />" \
-			% (action_url, num_digits, finish_on_key, timeout, method)
-		return self
-		
-	def record(self, action_url, max_length=60, finish_on_key='#', timeout=5, method='POST', transcribe=False, transcribe_callback='', play_beep='false'):
-		if transcribe:
-			self.content += "<Record action='%s' maxLength='%d' finishOnKey='%s' timeout='%d' method='%s' transcribe='%s', transcribeCallback='%s' />" \
-				% (action_url, max_length, finish_on_key, timeout, method, play_beep, transcribe, transcribe_calback)
-		else:
-			self.content += "<Record action='%s' maxLength='%d' finishOnKey='%s' timeout='%d' method='%s' playBeep='%s' />" \
-				% (action_url, max_length, finish_on_key, timeout, method, play_beep)
-				
-		return self
-		
-	def hangup(self):
-		self.content += '<Hangup />'
-		return self
-		
-	def dial(self):
-		raise NotImplementedError
+	def __str__(self):
+		return self.getcontent()
 		
 	def getcontent(self):
-		return "<?xml version='1.0'?><Response>%s</Response>" % (self.content)
+		return str(self.twilio_response)
+			
+	def get_last_verb(self):
+		return self.last_verb
+			
+	def say(self, to_say, voice=VOICE_WOMAN, language=LANGUAGE_ENGLISH, 
+			loop=0):
+		self.last_verb = twilio_official.Say(to_say, voice, language, loop)
+		self.twilio_response.append(self.last_verb)
 		
-	def printcontent(self):
-		print self.getcontent()
+		return self
+			
+	def play(self, to_play, loop=0):
+		self.last_verb = twilio_official.Play(to_play, loop)
+		self.twilio_response.append(self.last_verb)
+		
+		return self
+			
+	def pause(self, length):
+		self.last_verb = twilio_official.Pause(length)
+		self.twilio_response.append(self.last_verb)
+		
+		return self
+			
+	def redirect(self, url, method=HTTP_METHOD_GET):
+		self.last_verb = twilio_official.Redirect(url, method)
+		self.twilio_response.append(self.last_verb)
+		
+		return self
+			
+	def hangup(self):
+		self.last_verb = twilio_official.Hangup()
+		self.twilio_response.append(self.last_verb)
+		
+		return self
+			
+	def gather(self, action_url, method=HTTP_METHOD_GET, num_digits=1, 
+				finish_on_key='#', timeout=5):
+		self.last_verb = twilio_official.Gather(action_url, method, num_digits, \
+			timeout, finish_on_key)
+		self.twilio_response.append(self.last_verb)
+		
+		return self
+			
+	def number(self, number, send_digits=None):
+		self.last_verb = twilio_official.Number(number, send_digits)
+		self.twilio_response.append(self.last_verb)
+		
+		return self
+		
+	def sms(self, msg, receiver_num, sender_num, method=None, action_url=None, 
+			status_callback=None):
+		self.last_verb = twilio_official.Sms(msg, receiver_num, sender_num, \
+			method, action_url, status_callback)
+		self.twilio_response.append(self.last_verb)
+		
+		return self
+		
+	def conference(self, name, muted=None, beep=None, start_on_enter=None, 
+					end_on_exit=None, wait_url=None, wait_method=None):
+		self.last_verb = twilio_official.Conference(name, muted, beep,\
+			start_on_enter, end_on_exit, wait_url, wait_method)
+		self.twilio_response.append(self.last_verb)
+		
+		return self
+		
+	def dial(self, number, action=None, method=None):
+		self.last_verb = twilio_official.Dial(number, action, method)
+		self.twilio_response.append(self.last_verb)
+		
+		return self
+		
+	def record(self, action=None, method=None, max_length=None, timeout=None,
+				transcribe_callback=None, finish_on_key="1234567890*#"):
+		if transcribe_callback:
+			self.last_verb = twilio_official.Record(action, method, max_length, \
+				timeout, transcribeCallback=transcribe_callback, \
+				transcribe="true", finishOnKey=finish_on_key)
+		else:
+			self.last_verb = twilio_official.Record(action, method, max_length, \
+				timeout, finishOnKey=finish_on_key)	
+		self.twilio_response.append(self.last_verb)
+		
+		return self
+		
+	def reject(self, reason=REASON_REJECTED):
+		self.last_verb = twilio_official.Reject(reason)
+		self.twilio_response.append(self.last_verb)
+		
+		return self
